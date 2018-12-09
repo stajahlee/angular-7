3e19aeecfa121b17d87add647c71d102c18bd830
Stajah Lee Hoeflich <stajah@Stajahs-MacBook-Pro.local>
2018-12-09 02:12:08 -0500

    directory restructure

---
+++ react/ReactCoin/react-coin/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/common/Header';
+import List from './components/list/List';
+import './index.css'
+
+const App = () => {
+  // const title = 'React Coin';
+
+  return (
+    <div>
+      <Header />
+      
+      <List />
+    
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
